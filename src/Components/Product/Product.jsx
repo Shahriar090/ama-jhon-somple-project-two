d9fc4d5c56c5cae90fa92b944b673bf75f8b9576
Shahriar090 <shahriarh655@gmail.com>
2023-03-28 21:02:59 +0600

    added some cart details

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import './Product.css';
 const Product = (props) => {
     // console.log(props.product);
-    const {img, name, seller, quantity, price} = props.product;
+    const {img, name, seller, ratings, price} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
+           <div className='product-info'>
+           <h3 className='product-name'>{name}</h3>
+            <p>Price : ${price}</p>
+            <p>Manufacturer : {seller}</p>
+            <p>Rating : {ratings} Star</p>
+           
+           </div>
+           <button className='add-cart'>Add To Cart</button>
+            
+            
+            
         </div>
     );
 };
