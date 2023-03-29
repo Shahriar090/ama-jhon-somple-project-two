55d98c6cd9e4c75c8b6e023a5bf02743190ee8b0
Shahriar090 <shahriarh655@gmail.com>
2023-03-30 01:07:34 +0600

    added cart component and calculation

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -5,18 +5,22 @@ const Cart = (props) => {
     // another option is ({props}) means directly destructuring..
     // or const cart = props.cart..//
     const {cart} = props;
-    let total = 0;
+    let totalPrice = 0;
+    let totalShipping = 0;
     for(const product of cart){
-        total = total + product.price;
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping+product.shipping;
     }
+    const tax = totalPrice*7/100;
+    const grandTotal = totalPrice+totalShipping+tax;
     return (
         <div className='cart'>
              <h4>Order summary</h4>
         <p>Selected items : {cart.length}</p>
-        <p>Total Price : ${total}</p>
-        <p>Total Shipping : </p>
-        <p>Tax : </p>
-        <h4>Grand Total : </h4>
+        <p>Total Price : ${totalPrice}</p>
+        <p>Total Shipping : ${totalShipping}</p>
+        <p>Tax : ${tax.toFixed(2)}</p>
+        <h4>Grand Total : ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
